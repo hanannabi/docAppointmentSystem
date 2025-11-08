9ac35d030f78db4bf0da319eecf6963e4d2f28c2
hanannabi <hananjan1234@gmail.com>
2025-11-08 14:33:53 +0530

    count doctor appointment

--- src/main/java/com/example/mockitoJunit/dto/DoctorCountRequest.java
+++ src/main/java/com/example/mockitoJunit/dto/DoctorCountRequest.java
@@ -2,7 +2,6 @@ package com.example.mockitoJunit.dto;
 
 public class DoctorCountRequest {
     private Long doctorId;
-    private Integer count;
 
     public Long getDoctorId() {
         return doctorId;
@@ -10,13 +9,5 @@ public class DoctorCountRequest {
 
     public void setDoctorId(Long doctorId) {
         this.doctorId = doctorId;
-    }
-
-    public Integer getCount() {
-        return count;
-    }
-
-    public void setCount(Integer count) {
-        this.count = count;
     }
 }
